6da98d2663fe70a25fb3a2478831b1aa0c5000fc
Param savjani <paramsavjani3010@gmail.com>
2024-06-09 13:20:34 +0530

    f

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,111 +1,81 @@
 "use client";
 
-import { useState } from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./login.css";
-import { signIn, useSession } from "next-auth/react";
-import { Session } from "inspector";
+import Button from "@/components/ui/Button";
+import { FC, useState } from "react";
+import { signIn } from "next-auth/react";
+import { toast } from "react-hot-toast";
 
-const SignInSignUp = () => {
-    const [isSignUp, setIsSignUp] = useState(false);
-    const Session = useSession();
+const Page: FC = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const toggleSignUp = () => setIsSignUp(true);
-    const toggleSignIn = () => setIsSignUp(false);
-
-    const signwithGoogle = async () => {
+    async function loginWithGoogle() {
+        setIsLoading(true);
         try {
-           await signIn("google");
-        } catch (e) {
-            console.log(e);
+            await signIn("google");
+        } catch (error) {
+            console.log("Someting went wrong to login with google");
         } finally {
-            console.log(Session);
+            setIsLoading(false);
         }
-    };
+    }
 
     return (
-        <div>
-            <div
-                className={`container ${isSignUp ? "right-panel-active" : ""}`}
-                id="container"
-            >
-                <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <div className="social-container">
-                            <a onClick={signwithGoogle} href="#" className="social">
-                                <i className="fab fa-google"></i>
-                            </a>
-                            <a href="#" className="social">
-                                <i className="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#" className="social">
-                                <i className="fab fa-linkedin-in"></i>
-                            </a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button type="button">Sign Up</button>
-                    </form>
-                </div>
-                <div className="form-container sign-in-container">
-                    <form action="#">
-                        <h1>Sign in</h1>
-                        <div className="social-container">
-                            <a href="#" className="social">
-                                <i className="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#" className="social">
-                                <i className="fab fa-google"></i>
-                            </a>
-                            <a href="#" className="social">
-                                <i className="fab fa-linkedin-in"></i>
-                            </a>
-                        </div>
-                        <span>or use your account</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <a href="#">Forgot your password?</a>
-                        <button type="button">Sign In</button>
-                    </form>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>
-                                To keep connected with us please login with your
-                                personal info
-                            </p>
-                            <button
-                                className="ghost"
-                                id="signIn"
-                                onClick={toggleSignIn}
-                            >
-                                Sign In
-                            </button>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>
-                                Enter your personal details and start journey
-                                with us
-                            </p>
-                            <button
-                                className="ghost"
-                                id="signUp"
-                                onClick={toggleSignUp}
-                            >
-                                Sign Up
-                            </button>
-                        </div>
+        <>
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full flex flex-col items-center max-w-md space-y-8">
+                    <div className="flex flex-col items-center gap-8">
+                        <img
+                            className="h-12 w-auto"
+                            src="/logo-of-app.png"
+                            alt="Workflow"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                            Sign in to your account
+                        </h2>
                     </div>
+
+                    <Button
+                        isLoading={isLoading}
+                        type="button"
+                        className="max-w-sm mx-auto w-full"
+                        onClick={loginWithGoogle}
+                    >
+                        {isLoading ? null : (
+                            <svg
+                                className="mr-2 h-4 w-4"
+                                aria-hidden="true"
+                                focusable="false"
+                                data-prefix="fab"
+                                data-icon="github"
+                                role="img"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                                    fill="#4285F4"
+                                />
+                                <path
+                                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                                    fill="#34A853"
+                                />
+                                <path
+                                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                                    fill="#FBBC05"
+                                />
+                                <path
+                                    d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                                    fill="#EA4335"
+                                />
+                                <path d="M1 1h22v22H1z" fill="none" />
+                            </svg>
+                        )}
+                        Google
+                    </Button>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
-export default SignInSignUp;
+export default Page;
