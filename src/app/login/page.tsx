58339eff4c4c0a51a20cf0d76ce5482acf5f4582
Param savjani <paramsavjani3010@gmail.com>
2024-06-09 13:22:07 +0530

    f

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/ui/Button";
+import Image from "next/image";
 import { FC, useState } from "react";
 import { signIn } from "next-auth/react";
 import { toast } from "react-hot-toast";
@@ -24,10 +25,12 @@ const Page: FC = () => {
             <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div className="w-full flex flex-col items-center max-w-md space-y-8">
                     <div className="flex flex-col items-center gap-8">
-                        <img
-                            className="h-12 w-auto"
+                        <Image
+                            className=""
                             src="/logo-of-app.png"
                             alt="Workflow"
+                            width={300}
+                            height={300}
                         />
                         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                             Sign in to your account
