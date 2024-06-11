a097561cd06a4a02333246ce9a5e262283a1eace
Param savjani <paramsavjani3010@gmail.com>
2024-06-12 00:08:29 +0530

    f

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,36 +1,63 @@
 import { AddFriendSchema } from "@/lib/validation/add-friend";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { fetchRedis } from "@/helpers/redis";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
     try {
         const body = await req.json();
         const { email: emailToAdd } = AddFriendSchema.parse(body.email);
-        const RESTResponce = await fetch(
-            `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
-                },
-                cache: "no-cache",
-            }
-        );
 
-        const data = (await RESTResponce.json()) as { result: string | null };
-        const idToAdd = data.result;
+        const idToAdd = (await fetchRedis(
+            "get",
+            `user:email:${emailToAdd}`
+        )) as string;
 
         if (!idToAdd) {
-            return new Response("This person does not exist.", { status: 400 });
+            return new Response("This person does have account in Chatter Sphere.", { status: 400 });
         }
         const session = await getServerSession(authOptions);
         if (!session) {
-            return new Response("Unauthorized", { status: 401 });
+            return new Response("Please login first", { status: 401 });
         }
+
         if (idToAdd === session.user.id) {
             return new Response("You cannot add yourself as a friend", {
                 status: 400,
             });
         }
 
-    } catch (error) {}
+        const isAlreadyAdded = (await fetchRedis(
+            "sismember",
+            `user:${idToAdd}:incoming_friend_requests`,
+            session.user.id
+        )) as 0 | 1;
+
+        if (isAlreadyAdded) {
+            return new Response("You have already sent a friend request to this person", { status: 400 });
+        }
+
+        // check if user is already added
+        const isAlreadyFriends = (await fetchRedis(
+            "sismember",
+            `user:${session.user.id}:friends`,
+            idToAdd
+        )) as 0 | 1;
+
+        if (isAlreadyFriends) {
+            return new Response("Already friends with this user", {
+                status: 400,
+            });
+        }
+
+        await db.sadd(
+            `user:${idToAdd}:incoming_friend_requests`,
+            session.user.id
+        );
+
+        return new Response("ok");
+    } catch (error) {
+        
+    }
 }
--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -31,7 +31,9 @@ const AddFriendButton: FC<AddFriendButtonProps> = () => {
             setLoading(true);
             const validatedEmail = AddFriendSchema.parse({ email });
 
-            await axios.post("/api/friends/add", { email: validatedEmail });
+            const res = await axios.post("/api/friends/add", {
+                email: validatedEmail,
+            });
 
             setShowSuccessState(true);
         } catch (error) {
@@ -45,7 +47,8 @@ const AddFriendButton: FC<AddFriendButtonProps> = () => {
         if (error instanceof z.ZodError) {
             setError("email", { message: error.message });
         } else if (error instanceof AxiosError) {
-            setError("email", { message: error.response?.data.message });
+            setError("email", { message: error.response?.data });
+            console.log(error)
         } else {
             setError("email", { message: "Something went wrong." });
         }
@@ -60,13 +63,6 @@ const AddFriendButton: FC<AddFriendButtonProps> = () => {
             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-indigo-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-lg"></div>
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-lg sm:p-20">
-                    {/* <Image
-                        src="/svg-for-app.svg"
-                        style={{ filter:"invert(1)"}}
-                        alt="Logo"
-                        width={200}
-                        height={200}
-                    /> */}
                     <h2 className="text-2xl font-extrabold text-center mb-6">
                         Add a Friend
                     </h2>
@@ -83,7 +79,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = () => {
                         <div className="mt-3 flex gap-4">
                             <input
                                 {...register("email")}
-                                type="email"
+                                type="text"
                                 className={`block w-full rounded-md py-2 px-3 border ${
                                     errors.email
                                         ? "border-red-500"
@@ -98,11 +94,9 @@ const AddFriendButton: FC<AddFriendButtonProps> = () => {
                                 {loading ? "Adding..." : "Add"}
                             </Button>
                         </div>
-                        {errors.email && (
-                            <p className="mt-2 text-sm text-red-600 animate-fade-in">
-                                {errors.email.message}
-                            </p>
-                        )}
+                        <p className="mt-2 text-sm text-red-600 animate-fade-in">
+                            {errors.email?.message}
+                        </p>
                         {showSuccessState && (
                             <p className="mt-2 text-sm text-green-600 animate-fade-in">
                                 Friend request sent!
