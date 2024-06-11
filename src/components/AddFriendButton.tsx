5410bba0929a055498b8ed0a1a8083e92e2dcdee
Param savjani <paramsavjani3010@gmail.com>
2024-06-11 20:40:59 +0530

    f

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,66 +1,115 @@
 "use client";
 
 import { AddFriendSchema } from "@/lib/validation/add-friend";
-import { zodResolver } from "@hookform/resolvers/zod";
-import axios, { Axios, AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import { FC, useState } from "react";
-import { useForm } from "react-hook-form";
+import Button from "./ui/Button";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 
 interface AddFriendButtonProps {}
 
-const AddFriendButton: FC<AddFriendButtonProps> = () => {
-    const [showSuccessState, setShowSuccessState] = useState(false);
+type FormData = z.infer<typeof AddFriendSchema>;
 
-    const {} = useForm({ resolver: zodResolver(AddFriendSchema) });
+const AddFriendButton: FC<AddFriendButtonProps> = () => {
+    const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(AddFriendSchema),
+    });
 
     const addFriend = async (email: string) => {
         try {
-            const validatemail = AddFriendSchema.parse({ email });
-            await axios.post("/api/friends/add", { email: validatemail });
+            setLoading(true);
+            const validatedEmail = AddFriendSchema.parse({ email });
+
+            await axios.post("/api/friends/add", { email: validatedEmail });
+
             setShowSuccessState(true);
         } catch (error) {
-            if (error instanceof z.ZodError) {
-                console.log(error);
-                return;
-            }
-            if (error instanceof AxiosError) {
-                console.log(error);
-                return;
-            }
+            handleError(error);
+        } finally {
+            setLoading(false);
         }
     };
+
+    const handleError = (error: unknown) => {
+        if (error instanceof z.ZodError) {
+            setError("email", { message: error.message });
+        } else if (error instanceof AxiosError) {
+            setError("email", { message: error.response?.data.message });
+        } else {
+            setError("email", { message: "Something went wrong." });
+        }
+    };
+
+    const onSubmit = (data: FormData) => {
+        addFriend(data.email);
+    };
+
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-gray-800 to-blue-600">
-            <div className="container bg-black p-10 rounded-xl shadow-xl text-center max-w-md w-full">
-                <h1 className="title text-2xl font-bold text-gray-200 mb-6">
-                    Add Friends...
-                </h1>
-                <p className="description text-sm text-gray-300 mb-4">
-                    Connect with your friends by sending them friend requests.
-                    Once accepted, you&apos;ll become friends!
-                </p>
-                <form className="space-y-4">
-                    <label
-                        htmlFor="email"
-                        className="block text-sm font-medium text-gray-300 mb-2"
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-6 sm:py-12">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-indigo-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-lg"></div>
+                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-lg sm:p-20">
+                    {/* <Image
+                        src="/svg-for-app.svg"
+                        style={{ filter:"invert(1)"}}
+                        alt="Logo"
+                        width={200}
+                        height={200}
+                    /> */}
+                    <h2 className="text-2xl font-extrabold text-center mb-6">
+                        Add a Friend
+                    </h2>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="max-w-sm mx-auto"
                     >
-                        Add friend by E-Mail
-                    </label>
-                    <div className="flex gap-4">
-                        <input
-                            type="email"
-                            className="block w-full rounded-md py-2 px-3 bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            placeholder="you@example.com"
-                        />
-                        <button
-                            type="submit"
-                            className="button font-poppins flex items-center justify-center px-4 py-2 bg-blue-700 text-white font-semibold rounded-md hover:bg-blue-600 transition-transform transform hover:scale-105"
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-gray-700 mb-1"
                         >
-                            Add
-                        </button>
-                    </div>
-                </form>
+                            Add friend by E-Mail
+                        </label>
+                        <div className="mt-3 flex gap-4">
+                            <input
+                                {...register("email")}
+                                type="email"
+                                className={`block w-full rounded-md py-2 px-3 border ${
+                                    errors.email
+                                        ? "border-red-500"
+                                        : "border-gray-300"
+                                } shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300 ease-in-out transform hover:-translate-y-1`}
+                                placeholder="you@example.com"
+                            />
+                            <Button
+                                disabled={loading}
+                                className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 disabled:opacity-50 transition duration-300 ease-in-out transform hover:scale-105"
+                            >
+                                {loading ? "Adding..." : "Add"}
+                            </Button>
+                        </div>
+                        {errors.email && (
+                            <p className="mt-2 text-sm text-red-600 animate-fade-in">
+                                {errors.email.message}
+                            </p>
+                        )}
+                        {showSuccessState && (
+                            <p className="mt-2 text-sm text-green-600 animate-fade-in">
+                                Friend request sent!
+                            </p>
+                        )}
+                    </form>
+                </div>
             </div>
         </div>
     );
