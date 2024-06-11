d4e32af003f28aebbb574c9d3be92ef6922a9f23
Param savjani <paramsavjani3010@gmail.com>
2024-06-12 01:17:16 +0530

    f

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -7,14 +7,19 @@ interface LayoutProps {
     children: ReactNode;
 }
 
- const Layout = async ({ children }: LayoutProps) => {
+const Layout = async ({ children }: LayoutProps) => {
     const session = await getServerSession(authOptions);
     if (!session) {
         notFound();
     }
 
-    return <div>{children}</div>;
+    return (
+        <div className="w-full h-screen flex ">
+            <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-gray-200 bg-white">
+            </div>
+                {children}
+        </div>
+    );
 };
-
 
 export default Layout;
