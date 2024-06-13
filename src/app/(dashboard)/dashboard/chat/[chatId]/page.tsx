4c1f4c324d5ac0a686b461abbf7b307581f0fa8c
Param savjani <paramsavjani3010@gmail.com>
2024-06-13 17:33:26 +0530

    f

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,7 +3,7 @@ import { authOptions } from "@/lib/auth";
 import { messageArrayValidator } from "@/lib/validation/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
-import { FC } from "react";
+import Image from "next/image";
 
 interface pageProps {
     params: {
@@ -52,13 +52,46 @@ const page = async ({ params }: pageProps) => {
     }
 
     const chatPartnerId = userId1 === user.id ? userId2 : userId1;
-    const chatPartner = (await fetchRedis(
+    const chatPartnerString = (await fetchRedis(
         "get",
         `user:${chatPartnerId}`
-    )) as User;
+    )) as string;
+    const chatPartner = JSON.parse(chatPartnerString) as User;
     const initialMessages = await getChatMessages(chatId);
 
-    return <div>{params.chatId}</div>;
+    return (
+        <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)] bg-gradient-to-b from-gray-100 to-gray-200">
+            <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200 shadow-md bg-white">
+                <div className="relative flex items-center space-x-4 px-4">
+                    <div className="relative">
+                        <div className="relative w-10 sm:w-14 h-10 sm:h-14">
+                            <Image
+                                fill
+                                referrerPolicy="no-referrer"
+                                src={chatPartner.image}
+                                alt={`${chatPartner.name} profile picture`}
+                                className="rounded-full border-2 border-gray-300 shadow-lg"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col leading-tight">
+                        <div className="text-2xl flex items-center">
+                            <span className="text-gray-800 mr-3 font-bold">
+                                {chatPartner.name}
+                            </span>
+                        </div>
+
+                        <span className="text-sm text-gray-500">
+                            {chatPartner.email}
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    );
 };
 
 export default page;
