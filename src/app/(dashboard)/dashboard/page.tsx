70509acd665c2461862d7c77a055095ad3106093
Param savjani <paramsavjani3010@gmail.com>
2024-06-14 05:59:28 +0530

    f

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 const page = async () => {
     const session = await getServerSession(authOptions);
-    return <pre>{JSON.stringify(session, null, 2)}</pre>;
+    return <div>Dashboard</div>;
 };
 
 export default page;
