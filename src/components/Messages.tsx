5a460ee1683cb43cd5d4c2716b4e7be6c2f39e62
Param savjani <paramsavjani3010@gmail.com>
2024-06-18 05:59:23 +0530

    f

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -53,7 +53,7 @@ const Messages: FC<MessagesProps> = ({
     return (
         <div
             id="messages"
-            className="flex h-full flex-1 flex-col-reverse gap-1 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+            className="flex h-full flex-1 flex-col-reverse gap-1 px-4 pb-2 pt-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
         >
             <div ref={scrollDownRef} />
 
